aec44924244e7efbeb784c0f330e64176e308a9f
Brandon Luna <brandon.lunaj@gmail.com>
2026-07-12 00:51:09 -0400

    added a new components file

---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,29 @@
+import { Menu, Award } from "lucide-react";
+import { Link } from "@tanstack/react-router";
+
+export default function Navbar() {
+  return (
+    <nav className="shadow-2xs max-w-7xl rounded mx-auto">
+      <div className="flex justify-between p-4 border-b border-b-gray-200">
+        <div className="font-bold flex flex-row gap-1">
+          <Award />
+          <Link to="/" className="cursor-pointer">
+            Bench
+          </Link>
+        </div>
+        <ul className="hidden sm:flex flex-row gap-4 cursor-pointer font-bold">
+          <li>
+            <Link to="/players">Players</Link>
+          </li>
+          <li>
+            <Link to="/games">Games</Link>
+          </li>
+          <li>
+            <Link to="/teams">Teams</Link>
+          </li>
+        </ul>
+        <Menu className="sm:hidden cursor-pointer" />
+      </div>
+    </nav>
+  );
+}
