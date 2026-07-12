b369bccbdd9d1cffc6a0057b91571d028e9ac6a4
Brandon Luna <brandon.lunaj@gmail.com>
2026-07-12 00:50:27 -0400

    new page and made navbar visible on every page

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,28 +1,33 @@
-import { Outlet, createRootRoute } from '@tanstack/react-router'
-import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
-import { TanStackDevtools } from '@tanstack/react-devtools'
+import { Outlet, createRootRoute } from "@tanstack/react-router";
+import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
+import { TanStackDevtools } from "@tanstack/react-devtools";
+import Navbar from "#/components/Navbar";
 
-import '../styles.css'
+import "../styles.css";
 
 export const Route = createRootRoute({
   component: RootComponent,
-})
+});
 
 function RootComponent() {
   return (
     <>
-      <Outlet />
-      <TanStackDevtools
-        config={{
-          position: 'bottom-right',
-        }}
-        plugins={[
-          {
-            name: 'TanStack Router',
-            render: <TanStackRouterDevtoolsPanel />,
-          },
-        ]}
-      />
+      <Navbar />
+      <div className="max-w-7xl mx-auto">
+        <Outlet />
+
+        <TanStackDevtools
+          config={{
+            position: "bottom-right",
+          }}
+          plugins={[
+            {
+              name: "TanStack Router",
+              render: <TanStackRouterDevtoolsPanel />,
+            },
+          ]}
+        />
+      </div>
     </>
-  )
+  );
 }
