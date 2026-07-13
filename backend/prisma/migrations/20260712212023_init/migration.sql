81bed80f332a62e37c30e46c7768eb06103493d3
Brandon Luna <brandon.lunaj@gmail.com>
2026-07-13 12:01:25 -0400

    deleted migrations

--- backend/prisma/migrations/20260712212023_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Player" (
-    "id" TEXT NOT NULL,
-    "firstName" TEXT NOT NULL,
-    "lastName" TEXT NOT NULL,
-
-    CONSTRAINT "Player_pkey" PRIMARY KEY ("id")
-);
