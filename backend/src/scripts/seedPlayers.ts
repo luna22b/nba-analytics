09552bf514dc8d993af9ab02bdc8c38b8fd517be
Brandon Luna <brandon.lunaj@gmail.com>
2026-07-13 12:03:38 -0400

    fetches players and seeding database - fetches the data and extracts the fields then populates the database

--- backend/src/scripts/seedPlayers.ts
+++ backend/src/scripts/seedPlayers.ts
@@ -3,8 +3,10 @@ import { PrismaPg } from "@prisma/adapter-pg";
 import {
   fetchAllPlayerRefs,
   fetchPlayerDetails,
+  fetchPlayerStats,
 } from "../services/espn/playerService";
 import "dotenv/config";
+import { extractTeamId, extractPlayerStats } from "../utils/playerUtils";
 
 const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
 const prisma = new PrismaClient({ adapter });
@@ -22,9 +24,27 @@ async function main() {
 
   console.log(`Fetched ${players.length} player details`);
 
+  const stats = await fetchPlayerStats(players);
+
+  const cleanedStats = stats.map(extractPlayerStats);
+
   console.log("Saving players to database...");
 
   for (const player of players) {
+    const teamId = player.team ? extractTeamId(player.team.$ref) : null;
+
+    const teamExists = teamId
+      ? await prisma.team.findUnique({
+          where: { id: teamId },
+          select: { id: true },
+        })
+      : null;
+
+    console.log({
+      player: player.fullName,
+      teamId,
+    });
+
     await prisma.player.upsert({
       where: {
         id: player.id,
@@ -34,34 +54,104 @@ async function main() {
         firstName: player.firstName,
         lastName: player.lastName,
         fullName: player.fullName,
+
         height: player.height,
         weight: player.weight,
         age: player.age,
-        active: player.active,
+
+        dateOfBirth: player.dateOfBirth ? new Date(player.dateOfBirth) : null,
+
+        debutYear: player.debutYear,
+        experienceYears: player.experience?.years,
+
         position: player.position?.name,
         positionAbbr: player.position?.abbreviation,
+
         jerseyNumber: player.jersey,
+
+        birthCity: player.birthPlace?.city,
+        birthState: player.birthPlace?.state,
+        birthCountry: player.birthPlace?.country,
+
         headshotUrl: player.headshot?.href,
+
+        active: player.active,
+
+        team: teamExists
+          ? {
+              connect: {
+                id: teamId!,
+              },
+            }
+          : undefined,
       },
 
       create: {
         id: player.id,
+
         firstName: player.firstName,
         lastName: player.lastName,
         fullName: player.fullName,
+
         height: player.height,
         weight: player.weight,
         age: player.age,
-        active: player.active,
+
+        dateOfBirth: player.dateOfBirth ? new Date(player.dateOfBirth) : null,
+
+        debutYear: player.debutYear,
+        experienceYears: player.experience?.years,
+
         position: player.position?.name,
         positionAbbr: player.position?.abbreviation,
+
         jerseyNumber: player.jersey,
+
+        birthCity: player.birthPlace?.city,
+        birthState: player.birthPlace?.state,
+        birthCountry: player.birthPlace?.country,
+
         headshotUrl: player.headshot?.href,
+
+        active: player.active,
+
+        team: teamExists
+          ? {
+              connect: {
+                id: teamId!,
+              },
+            }
+          : undefined,
       },
     });
   }
 
   console.log("Finished saving players!");
+
+  console.log("Saving player stats...");
+
+  for (const stat of cleanedStats) {
+    await prisma.playerStats.upsert({
+      where: {
+        playerId: stat.playerId,
+      },
+
+      update: {
+        ppg: stat.ppg,
+        fgPct: stat.fgPct,
+        threePointPct: stat.threePointPct,
+      },
+
+      create: {
+        playerId: stat.playerId,
+        ppg: stat.ppg,
+        fgPct: stat.fgPct,
+        threePointPct: stat.threePointPct,
+      },
+    });
+  }
+
+  console.log("Finished saving stats!");
 }
 
 main()
