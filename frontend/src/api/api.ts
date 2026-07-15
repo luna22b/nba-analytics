66ff4c52b39948de058bb8e5c9d6f51c486c75a1
Brandon Luna <brandon.lunaj@gmail.com>
2026-07-15 14:54:49 -0400

    added a new api for getting team or players

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -4,3 +4,18 @@ export async function getTeams() {
   const res = await axios.get("http://localhost:5000/api/teams");
   return res.data;
 }
+
+export async function getSpecificTeam(teamId: string) {
+  const res = await fetch(`http://localhost:5000/api/teams/${teamId}`);
+  return res.json();
+}
+
+export async function getPlayers() {
+  const res = await axios.get("http://localhost:5000/api/players");
+  return res.data;
+}
+
+export async function getSpecificPlayer(playerId: string) {
+  const res = await axios.get(`http://localhost:5000/api/players/${playerId}`);
+  return res.data;
+}
