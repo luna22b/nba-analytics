3a2a89baabc226e6d63db6daadca89b04ea67f9a
Brandon Luna <brandon.lunaj@gmail.com>
2026-07-28 20:21:46 -0400

    removed hard-coded localhost

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -1,26 +1,27 @@
 import axios from "axios";
+import { API_URL } from "./config";
 
 export async function getTeams() {
-  const res = await axios.get("http://localhost:5000/api/teams");
+  const res = await axios.get(`${API_URL}/api/teams`);
   return res.data;
 }
 
 export async function getSpecificTeam(teamId: string) {
-  const res = await fetch(`http://localhost:5000/api/teams/${teamId}`);
+  const res = await fetch(`${API_URL}/api/teams/${teamId}`);
   return res.json();
 }
 
 export async function getPlayers() {
-  const res = await axios.get("http://localhost:5000/api/players");
+  const res = await axios.get(`${API_URL}api/players`);
   return res.data;
 }
 
 export async function getSpecificPlayer(playerId: string) {
-  const res = await axios.get(`http://localhost:5000/api/players/${playerId}`);
+  const res = await axios.get(`${API_URL}/api/players/${playerId}`);
   return res.data;
 }
 
 export async function getGames() {
-  const res = await axios.get("http://localhost:5000/api/games");
+  const res = await axios.get(`${API_URL}/api/games`);
   return res.data;
 }
