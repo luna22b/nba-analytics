5017b3c695c420964af802d641ce00952e2ad218
Brandon Luna <brandon.lunaj@gmail.com>
2026-07-28 20:57:31 -0400

    added a health check

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -5,3 +5,9 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "NBA Analytics API is running",
+  });
+});
