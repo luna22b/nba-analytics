8004b7d71f8eed1a1f1aa2ce1b3fa78d7c14f232
Brandon Luna <brandon.lunaj@gmail.com>
2026-07-28 20:25:21 -0400

    added a forward slash to api routes

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -2,26 +2,26 @@ import axios from "axios";
 import { API_URL } from "./config";
 
 export async function getTeams() {
-  const res = await axios.get(`${API_URL}/api/teams`);
+  const res = await axios.get(`/${API_URL}/api/teams`);
   return res.data;
 }
 
 export async function getSpecificTeam(teamId: string) {
-  const res = await fetch(`${API_URL}/api/teams/${teamId}`);
+  const res = await fetch(`/${API_URL}/api/teams/${teamId}`);
   return res.json();
 }
 
 export async function getPlayers() {
-  const res = await axios.get(`${API_URL}api/players`);
+  const res = await axios.get(`/${API_URL}api/players`);
   return res.data;
 }
 
 export async function getSpecificPlayer(playerId: string) {
-  const res = await axios.get(`${API_URL}/api/players/${playerId}`);
+  const res = await axios.get(`/${API_URL}/api/players/${playerId}`);
   return res.data;
 }
 
 export async function getGames() {
-  const res = await axios.get(`${API_URL}/api/games`);
+  const res = await axios.get(`/${API_URL}/api/games`);
   return res.data;
 }
